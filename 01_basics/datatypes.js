0ce59eb33aa7397f9d488dd1693660456e1f3fea
jiyadua <150153827+jiyadua@users.noreply.github.com>
2024-05-08 06:21:42 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+//  alert(3+3) //we are using Nodej, not browser
+
+let name="jiya";
+let age=20;
+let state;
+
+// number=>2 to power 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined=>
+    // symbol=>unique
+// object
+
+console.log( typeof "jiya") 
+console.log(typeof age)
