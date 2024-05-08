ab3e846fa58eaf22951de61c49280bcd7c5178d9
jiyadua <150153827+jiyadua@users.noreply.github.com>
2024-05-08 06:23:46 +0000

    variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,5 +2,16 @@ const accountID=123;
 let accountEmail="jiya@google.com";
 var accountPassword="2345";
 accountCity="jaipur";
+// accountID=5678;// Not allowed
+accountEmail="garv@google.com";
+accountPassword="5432";
+accountCity="Muzzafarnagar";
+let accountState;
 
 console.log(accountID);
+/*Prefer not to use var
+Because of issue in functional and global scope*/
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
