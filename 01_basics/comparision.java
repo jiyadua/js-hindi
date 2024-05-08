1420a68f483afc415a4e69f2296a10c32a411b7d
jiyadua <150153827+jiyadua@users.noreply.github.com>
2024-05-08 08:19:16 +0000

    container config done

---
+++ 01_basics/comparision.java
@@ -0,0 +1,16 @@
+console.log(2>1)
+console.log(2==2)
+console.log(2>=1)
+
+console.log("2">1)
+console.log("2"<1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log("2"===2)
+
+// in === there is no conversion
+// in == conversion is allowed
+
---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,35 @@
+let score=true;
+console.log(typeof(score))
+let valueInNnumber=Number(score)
+console.log(typeof(valueInNnumber));
+console.log(valueInNnumber);
+
+// "33"=>33
+// null=>0
+// undefined=>Nan
+// "33abc"=>Nan
+// true=>1;false=>0
+
+let isloggedIn=1;
+let booleanIsLoggedIn=Boolean(isloggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1=>true;0=>false
+// ""=>false
+// "jiya"=>true
+
+let someNumber=33
+let some=String(someNumber)
+console.log(some);
+console.log(typeof some)
+
+// ********************************Operations******************************************//
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**5)
+console.log(1+2+"2")
+console.log("1"+2+2)
